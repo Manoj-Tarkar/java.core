31cd9b81b4426fec119d04b8ad8a8eb0fef1ee30
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:52:56 +0530

    Add files via upload

---
+++ sal.java
@@ -0,0 +1,18 @@
+package manoj;
+
+import java.util.Scanner;
+
+public class sal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ob =new Scanner(System.in);
+		 System.out.println("Enter the coice two no");
+         int wag,wor;
+         wor=ob.nextInt();
+         wag=ob.nextInt();
+         System.out.println(wor*wag); 
+  
+	}
+
+}
