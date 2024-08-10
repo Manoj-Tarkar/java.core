00c70db5284b8b26a6f6b502d8713f795542a2ac
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:51:56 +0530

    Add files via upload

---
+++ PerimeterSquare.java
@@ -0,0 +1,16 @@
+package manoj;
+
+import java.util.Scanner;
+
+public class PerimeterSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ob =new Scanner(System.in);
+        System.out.println("Enter the first number :");
+        int a=ob.nextInt();
+        System.out.println(a*4);
+
+	}
+
+}
