1665096e18c059a4e2042e43d55284de85db10f8
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:54:13 +0530

    Add files via upload

---
+++ tem.java
@@ -0,0 +1,15 @@
+package manoj;
+
+import java.util.Scanner;
+
+public class tem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner ob =new Scanner(System.in);
+     int c=ob.nextInt();
+     float F=(c*9/5+32);
+      System.out.println("the tem in fahrenheit is :"+F); 
+	}
+
+}
