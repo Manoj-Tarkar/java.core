4388a666857e8335ed0d61d08cf179ff3842cb49
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:32:39 +0530

    Days of Week convert.java

---
+++ DaysOfWeek.java
@@ -0,0 +1,15 @@
+package manoj;
+
+import java.util.Scanner;
+
+public class DaysOfWeek {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ob =new Scanner(System.in);
+        System.out.println("Enter the number of weeks :");
+        int a=ob.nextInt();
+        System.out.println("The number of days is "+a*7);
+	}
+
+}
