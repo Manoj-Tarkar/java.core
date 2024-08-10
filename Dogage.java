7f4e135b556927a9caf43c55e2afe8d215ae6476
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:33:55 +0530

    human convert

---
+++ Dogage.java
@@ -0,0 +1,19 @@
+package manoj;
+
+import java.util.*;
+
+public class Dogage {
+
+	public Dogage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int human;
+	Scanner ob = new Scanner(System.in);
+	System.out.println("enter this age :");
+	human=ob.nextInt();
+    System.out.print("Enter your age"+human*7);
+	}
+
+}
