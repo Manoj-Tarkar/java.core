e59651698ba79bfdefc50c1831ca793941913088
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-10 19:52:18 +0530

    Add files via upload

---
+++ Rec.java
@@ -0,0 +1,17 @@
+package manoj;
+
+import java.util.Scanner;
+
+public class Rec {
+
+	public Rec() {
+	}
+		public static void main(String[] args) {
+			Scanner ob = new Scanner(System.in);
+			int w=ob.nextInt();
+			int b=ob.nextInt();
+			System.out.print(w*b);
+		}
+	}
+
+
