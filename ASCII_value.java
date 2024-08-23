c3264f8fcb8ef8dbd747844e60cc0ae2abb7a200
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-23 10:34:19 +0530

    Add files via upload

---
+++ ASCII_value.java
@@ -0,0 +1,14 @@
+package manoj1Loop;
+
+public class ASCII_value {
+void ascii(){
+	for(int ch=1;ch<=250;ch++){
+		System.out.print((char)ch);
+	}
+}
+	public static void main(String[] args) {
+		ASCII_value obj=new ASCII_value(); 
+		obj.ascii();
+	}
+
+}
