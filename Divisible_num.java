d3d85e2e72f0823ed28c2e974d206c51b333a70a
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2024-08-23 10:39:56 +0530

    Add files via upload

---
+++ Divisible_num.java
@@ -0,0 +1,21 @@
+package manoj1Loop;
+
+import java.util.Scanner;
+public class Divisible_num {
+void div_num(){
+	int sum=0;
+	for (int i=101;i<200;i++){
+		if (i%9==0){
+			
+			System.out.println(i);
+			
+		}
+	}
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Divisible_num obj=new Divisible_num();
+		obj.div_num();
+	}
+
+}
